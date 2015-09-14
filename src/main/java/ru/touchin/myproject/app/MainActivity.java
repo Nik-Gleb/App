dca1c78f39cc4de1f2783c8418668d0bb2a44acb
Nikitenko Gleb <gleb@e-magic.org>
2015-09-14 17:10:38 +0300

    Patched blah-blah

--- src/main/java/ru/touchin/myproject/app/MainActivity.java
+++ src/main/java/ru/touchin/myproject/app/MainActivity.java
@@ -20,7 +20,7 @@ public final class MainActivity extends Activity {
         if (BuildConfig.DEBUG)
             mRootView = getWindow().getDecorView()
                     .findViewById(android.R.id.content);
-        //remote changes2
+        //remote changes2 and my patch
     }
 
     @Override
