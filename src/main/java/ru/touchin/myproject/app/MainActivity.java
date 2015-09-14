99e1c196e2af44d999f606d039978caa15730bf0
Nikitenko Gleb <gleb@e-magic.org>
2015-09-14 16:11:17 +0300

    ch to change

--- src/main/java/ru/touchin/myproject/app/MainActivity.java
+++ src/main/java/ru/touchin/myproject/app/MainActivity.java
@@ -20,7 +20,7 @@ public final class MainActivity extends Activity {
         if (BuildConfig.DEBUG)
             mRootView = getWindow().getDecorView()
                     .findViewById(android.R.id.content);
-        //chng
+        //change
     }
 
     @Override
