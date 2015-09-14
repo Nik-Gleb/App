2c073c8b237ab630dd4f5caa12fa5eaa505313ff
Gleb Nikitenko <gleb@e-magic.org>
2015-09-14 20:40:07 +0300

    Update MainActivity.java

--- src/main/java/ru/touchin/myproject/app/MainActivity.java
+++ src/main/java/ru/touchin/myproject/app/MainActivity.java
@@ -20,7 +20,7 @@ public final class MainActivity extends Activity {
         if (BuildConfig.DEBUG)
             mRootView = getWindow().getDecorView()
                     .findViewById(android.R.id.content);
-        //remote change4
+        //remote change4.3
     }
 
     @Override
